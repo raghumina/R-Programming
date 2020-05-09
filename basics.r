5bf4bfa21ad6909abdaede8fba102639902199cb
hazard00 <42498911+hazard00@users.noreply.github.com>
2020-05-09 09:39:44 -0700

    Basic user input output in R

---
+++ basics.r
@@ -0,0 +1,84 @@
+2+2
+# a new r program
+print("yo new program")
+y <- 1:10
+y <- 5
+y <-c(1:10)
+y <- x <- 1:10
+x + y
+z <- x + y 
+z1 <- x * y 
+ls()
+rm(z1)
+remove z
+remove(z)
+rm(list = ())
+rm(list = (ls))
+rm(list = ls())
+
+browseURL("http://cran.r-project.org/web/views/")
+install.packages("LiblinearR")
+library()
+search()
+require("LiblinearR")
+
+
+g <- 20:20
+g <- a <- 10
+c=2,d=3:
+f=c+d
+
+
+a<-10
+b<-1:1
+d <- a + b
+a + b
+require("methods")
+requireMethods()
+require("foreign")
+require("LiblibrelR")
+require("datasets")
+search()
+require("datasets")
+search()
+require("datasets")
+library(help = "datasets")
+? iris
+str(iris)
+iris
+data("iris")
+
+x1 <- 1:10
+x2 <- c(1,2,3,4,5,6,7,8,9,0)
+x3 <- seq(7,70, by = 7)
+x4 <- seq(9,90, by = 9)
+x5 <- scan()
+Pdb <- read.table
+str(Pdb)
+<-read table("Pdb")
+Pdb <- read.csv(Pdb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x1 <- 1:10
+x2 <- 20:20
+x3 <- (1,2,3,4,3,2,1)
+x3 <- c(1,2,3,4,3,2,1)
+read.csv(Data)
+read.csv(Data.csv)
