2e35b91a2cf50dd0a3d139df685a28a3287bfe20
raghu <raghumina11@gmail.com>
2020-05-10 00:41:57 -0700

    Creating a Histogram in R

---
+++ Histogram.r
@@ -0,0 +1,11 @@
+print(" hello ")
+Student <- read.csv("C:/Users/ashok/R Data Imports/Student.csv",header = TRUE)
+View(Student)
+Student
+# CREATING HISTOGRAM IN R
+hist(Student$Age)
+hist(Student$Age, breaks = 10)
+
+# TO CHECK FREQUENCY DENSITY OR TO SPECIFIY THE CRITERIA 
+hist(Student$Age, breaks = 10,c(0,10,15,30), freq = TRUE,col = "blue")
+hist(Student$Age, breaks = 10,c(0,10,15,30), freq = TRUE,col = "blue",main  = "studet age")
