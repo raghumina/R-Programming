5d2a6107f6a78984c0ee938bf2a3cb939d0b6dbc
raghu <raghumina11@gmail.com>
2020-05-12 10:18:55 -0700

    LOOPS AND CONDITIONS IN R

--- LoopsAndConditions.r
+++ LoopsAndConditions.r
@@ -1,10 +1,88 @@
 # LOOPS AND CONDITIONS IN R 
+
+
 # IF CONDITION
-check <- 2
-if(check ==1)
+check <-1
+if(check == 1)
 {
-  print("yes")
+  print("hello")
 }
-else {
-  print("no")
+if(check == 2)
+{
+  print("Hello")
 }
+
+# IF ELSE Condition 
+# Used to check two conditions yes or no 
+check<-2
+ifelse(check==1,"True","False")
+
+
+
+# POWER OF IF ELSE LOOP
+# To test the values 
+testval<-c(0,1,1,0,1,0,1,0)
+ifelse(testval==1,"True","False")
+
+ifelse(testval==1, 5*testval, 3*testval)
+
+
+# Switch cases 
+lets.swtich <- function(x)
+{
+  switch(x, "a"="alpha","b"="beta","c"="cos","t"="theta","o"="omega","OUT OF BOUNDS")
+}  
+
+lets.swtich("a")
+lets.swtich("t")
+lets.swtich("w")
+
+# for 
+
+for(i in 1:10)
+{
+  print(i)
+}
+
+# power of print in R
+print(1:10)
+
+
+# While loop 
+ while(x <=20)
+ {
+   print(x)
+   x<- x+2
+ }
+
+
+# LOOP CONTROL STATEMENT 
+
+# NEXT STATEMENT 
+
+for(i in 1:10)
+{
+  if(i == 3)
+  {
+    next
+  }
+  print(i)
+}
+
+
+
+
+# Break Statement 
+
+for(i in 1:10)
+{
+  if(i==3)
+  {
+    break
+  }
+  print(i)
+} 
+
+
+
+# LOOPS COMPLETED 
