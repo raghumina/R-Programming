75cd833aa0d0b51c867e987863e38cae2f2f5fb3
raghu <raghumina11@gmail.com>
2020-05-13 09:03:36 -0700

    Update FunctionR.r

--- FunctionR.r
+++ FunctionR.r
@@ -11,3 +11,37 @@ celsius_to_kelvin <- function(temp_C) {
 
 # test a udf (user defined function )
 celsius_to_kelvin(300)
+
+
+# another example 
+say.name<-function()
+{
+  print("tommy")
+}
+
+say.name()
+
+# user defined function with multiparameter
+say.name<-function(a1,a2,a3)
+{
+  print(sprintf("HELLO %s %s %s",a1,a2,a3))
+}
+
+say.name("xyz","qwe","towo")
+
+
+
+
+# DO CALL FUNCTION 
+mean.it<-function(x ,func = mean)
+{
+  do.call(func, args = list(x))
+}
+
+mean.it(100:10000)
+mean.it(100:1000,sum)
+mean.it(10:100)
+
+
+
+  
