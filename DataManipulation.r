62982726531bbb2943a9fc63aa5c657d17a6ac2a
raghu <raghumina11@gmail.com>
2020-06-03 22:42:29 +0530

    Create DataManipulation.r

---
+++ DataManipulation.r
@@ -0,0 +1,12 @@
+# Data visualization - continous one variable
+
+# import a dataset and we will work on it 
+
+db <- read.csv("C:/Users/ashok/Desktop/BOOKS/Complete ML in R/1. Linear Regression/Movie_collection_test.csv", header = T)
+
+# to take only required amount of data from a large dataset 
+# we use 
+#   "db <- sample_frac(db,0.1)"
+
+
+
