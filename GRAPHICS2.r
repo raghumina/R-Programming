66db4378c6cf8fcaf644d2620a9b1f4a4a30559f
raghu <raghumina11@gmail.com>
2020-07-11 22:13:23 +0530

    Update GRAPHICS2.r

--- GRAPHICS2.r
+++ GRAPHICS2.r
@@ -4,3 +4,22 @@
 library(datasets)
 ?datasets
 library(help = "datasets")
+data.frame(mtcars)
+head(mtcars)
+View(mtcars)
+barplot(mtcars$disp)
+
+# NEED A TABLE WITH FREQUENCIES WITH EACH CATAGEROY
+cylinders <-table(mtcars$cyl)
+barplot(cylinders, col = "red")
+plot(cylinders)
+
+
+# HISTOGRAMS BY GROUPS 
+
+# PUT GRAPH IN 3 ROWS AND 1 COLOUMN
+
+par(mfrow = c(3, 1))
+
+# Histogram For each species using options 
+
