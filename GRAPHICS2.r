0e56cfdd1ae4cd8717f753fbad4a16843ec6d94c
raghu <raghumina11@gmail.com>
2020-07-11 22:37:33 +0530

    Update GRAPHICS2.r

--- GRAPHICS2.r
+++ GRAPHICS2.r
@@ -14,7 +14,7 @@ cylinders <-table(mtcars$cyl)
 barplot(cylinders, col = "red")
 plot(cylinders)
 
-
+View(iris)
 # HISTOGRAMS BY GROUPS 
 
 # PUT GRAPH IN 3 ROWS AND 1 COLOUMN
@@ -22,4 +22,15 @@ plot(cylinders)
 par(mfrow = c(3, 1))
 
 # Histogram For each species using options 
+
+hist(iris$Petal.Width [iris$Species == "verginica"], xlim = c(0, 3), breaks = 9,
+     col = "blue")
+
+
+hist(iris$Petal.Width [iris$Species == "versicolor"], xlim = c(0, 3), breaks = 9,
+     col = "yellow")
+
+
+hist(iris$Petal.Width [iris$Species == "setosa"], xlim = c(0, 3), breaks = 9,
+     col = "red")
 
