346db0198b1f41d83aa44b6b0cdbc943bec6b3e5
raghu <raghumina11@gmail.com>
2020-07-12 12:32:05 +0530

    Create Statistics.r

---
+++ Statistics.r
@@ -0,0 +1,11 @@
+library(datasets)
+head(iris)
+summary(iris)
+
+plot(iris)
+
+summary(iris$Sepal.Length)  # quantative variable 
+summary(iris$Species)   # categorical values 
+summary(iris)  # all of the data frame 
+
+
