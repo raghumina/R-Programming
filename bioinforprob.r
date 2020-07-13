ba3045ea73d9312058486e903f3dfbd18035b638
raghu <raghumina11@gmail.com>
2020-07-13 11:07:27 +0530

    Create bioinforprob.r

---
+++ bioinforprob.r
@@ -0,0 +1,31 @@
+# Load the required packages 
+library(stats)
+library(ggplot2)
+library(dplyr)
+
+
+# to create a data frame 
+# data frame sol which will consist of OBJECT . ATTRIBUTESX AND ATTRIBUTESY 
+
+Object <- c("Medicine-A","Medicine-B","Medicine-C","Medicine-D")
+Attributesx <- c(1,2,4,5)
+AttributesY <- c(1,1,3,4)
+sol <- data.frame(Object, Attributesx, AttributesY)
+View(sol)
+
+# NOW PLOTTING A SCATTER PLOT 
+plot(sol, col = "red")
+
+# plot of x and y axis 
+plot(sol$Attributesx, sol$AttributesY, col = "red",
+     xlab = "molecular weight",
+     ylab = "pH",
+     main = "Medicine")
+
+
+# k mean clustring 
+
+mydata <- select(sol,c(2,3))
+plot(mydata)
+
+KM = kmeans()
