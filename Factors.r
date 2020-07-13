0a87af5cc067b39f1b66a485f78143ae94e3d4e1
raghu <raghumina11@gmail.com>
2020-07-13 22:37:07 +0530

    Create Factors.r

---
+++ Factors.r
@@ -0,0 +1,16 @@
+# Factors 
+# 
+##
+
+# create a data 
+(x1 <- 1:3)
+(y <- 1:9)
+
+# combine variables 
+
+(df1 <- cbind.data.frame(x1, y))
+
+(typeof(df1$x1))
+(typeof(df1$y))     # both are of integer type 
+
+
