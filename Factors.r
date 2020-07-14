a6e8407242cc60f52aca8d00cdf7804950f8aae0
raghu <raghumina11@gmail.com>
2020-07-14 18:56:50 +0530

    Update Factors.r

--- Factors.r
+++ Factors.r
@@ -22,3 +22,56 @@
 
 
 (df2 <- cbind.data.frame(x2, y1))
+
+
+#########################################################################################################
+
+
+T <- 1:10     # form 1 to 10 
+print(T)
+
+
+t1 <- 10:1   # from 10 to 1 
+print(t1)
+
+# seq operator helps the varialbe run in a sequence 
+# seq = sequence
+
+?seq
+
+
+# SPECIFY CHANGE IN VALUES 
+
+(x4 <- seq(30, 0, by = -3))
+
+
+
+(x5 <- seq(0, 30, by = 3))
+
+
+# c = combine or concate 
+# ENTer multiple values with c 
+
+(x5 <- c(1,2,4,6,8,09))
+print(x5)
+
+
+# scan = read data live 
+# from the user 
+
+x6 <- scan()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
