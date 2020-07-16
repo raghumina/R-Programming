b90459168c49ce2aeda45d1e3dd11787eef7a00d
raghu <raghumina11@gmail.com>
2020-07-16 18:02:20 +0530

    Create Rrevision.r

---
+++ Rrevision.r
@@ -0,0 +1,19 @@
+# This file or folder is just created by me for revision
+# FROM 0 TO HERO :) :)
+# JUST REVISING MY BASICS 
+
+
+# 
+#  ASIIGNMENTS IN R 
+x = 1 
+print(x)    #   = SIGN CAN ALSO BE USED BUT ( <- ) THIS ASSIGNMET IS MORE PREFERABLE 
+
+x1 <- 12    # LIKE THIS ONE 
+print(x1)
+
+name = hello     # THE CHARACTER VALUES SHOULD BE IN ""
+print(name)
+
+name1 = "Jerry"
+print(name1)       # like this character values should be in ( "" ).
+
