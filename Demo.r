7f609ca3492c2cebeba20e831e6a5f873b6a5b94
raghu <raghumina11@gmail.com>
2020-07-22 15:12:35 +0530

    Update Demo.r

--- Demo.r
+++ Demo.r
@@ -3,6 +3,7 @@
 
 
 # creating a data frame 
+
 lengths <- c(12,14,15,13,13,16,19,20,20,17)
 codons <- c(3,4,2,3,3,3,4,5,6,4)
 orf <- c(2,1,2,2,2,1,1,4,3,2)
@@ -14,13 +15,26 @@ View(demo)    # to view the data frame
 
 plot(demo)    # basic scatter plot of demo 
 
-plot(demo$lengths,demo$codons, col = "red", xlab = "length",
-     ylab = "codons",
-     main = "plot of demo1")       # to creat aspecific plot 
+plot(demo$lengths,demo$codons,
+     col = "red",                  # color of clusters 
+     xlab = "length",              # x axis name 
+     ylab = "codons",              # y axis name 
+     main = "plot of demo1")       # to creat a specific plot 
                                    # with color, name, other criteria 
 
 # to create other plots or graphgs 
 # for example a histogram 
 
-hist(demo$orf,demo$gc_con, col = "blue ")
+hist(demo$orf)
+
+
+str(demo)                         # to see the structure of the data 
+
+
+d1 <- kmeans(demo, 4)
+print(d1)
+
+plot(d1$cluster, col = "red")
+
+ 
 
