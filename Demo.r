a948ce199dc7a6a7113f8b2e90b2eb954fcc86d3
raghu <raghumina11@gmail.com>
2020-07-23 22:14:45 +0530

    Update Demo.r

--- Demo.r
+++ Demo.r
@@ -1,18 +1,26 @@
 # Practical 1
 # K-mean clustring 
-
-
+# library(rio)                      # for r input and output
+library(stats)                      # for stastical data 
+library(ggplot2)                    # for data visvualization
+library(dplyr)                      # for data manipulation 
+library(ggfortify)                  # for data visvualization
+library(datasets)
+View(iris)
 # creating a data frame 
+# or we can import a data frame 
 
-lengths <- c(12,14,15,13,13,16,19,20,20,17)
+# import("File path/file name" )
+
+lengths <- c(12,14,15,13,13,16,19,20,20,17)  # variable creation
 codons <- c(3,4,2,3,3,3,4,5,6,4)
 orf <- c(2,1,2,2,2,1,1,4,3,2)
 gc_con <- c(4,5,6,7,8,9,7,6,4,3)
 demo <- data.frame(lengths, codons, orf, gc_con)
-
+?rio
 # Dataframe "demo" created 
 View(demo)    # to view the data frame 
-
+demo
 plot(demo)    # basic scatter plot of demo 
 
 plot(demo$lengths,demo$codons,
@@ -43,33 +51,43 @@ summary(demo)   # Summary of the data
 # k- mean is a unsupervised learning
 
 
-democluster <- kmeans(demo,4)
-print(democluster)
+democluster <- kmeans(demo,2)
+democluster
 plot(democluster)
 
 
-democluster4 <- kmeans(demo, centers = 4)
+democluster4 <- kmeans(demo, centers =2 )
 
-# Show the centres 
+# Show the centres are representative of particular observation in cluster s
 democluster4$centers
 democluster4$cluster
+democluster4$size
+democluster4$tot.withinss
+ggplot(demo)
 
 # Plot the groups 
 
-plot(demo [democluster4$cluster == 1,],
-     col = "red",
-     xlim=c( min(demo[,1]), max(demo[,1])),
-     ylim=c(min(demo[,2]), max(demo[,2]))
-      )
+autoplot(democluster4, demo, frame = TRUE)
+    
 
-points( demo [democluster4$cluster == 2, ],
-       col = "blue")
 
-points( demo [democluster4$cluster == 3, ],
-       col = "green")
 
-points( demo [democluster4$cluster == 4, ],
-       col = "yellow")
 
-points(democluster4$centers, pch = 2, col = "seagreen")
 
+
+
+
+# elobow method to determine the optimizable numbre of clusters 
+
+k.max <- 10    # max 10 cluster assumed 
+wss <- rep(NA, k.max)
+nClust <- list()
+for (i in 1:k.max) {
+   driveClasses <- kmeans(demo, i)
+   wss[i] <- driveClasses$tot.withinss
+   nClust[[i]] <- driveClasses$size
+}
+plot( 1:k.max, wss,
+      type = "b", pch = 10,
+      xlab = "Number of cluster K ",
+      ylab = "Witin total cluster sum: demo")
