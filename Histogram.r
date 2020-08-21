0869e1df801d3f1f90192c8e0417484c426c26a2
raghu <raghumina11@gmail.com>
2020-08-21 22:55:52 +0530

    Update Histogram.r

--- Histogram.r
+++ Histogram.r
@@ -9,3 +9,7 @@ hist(Student$Age, breaks = 10)
 # TO CHECK FREQUENCY DENSITY OR TO SPECIFIY THE CRITERIA 
 hist(Student$Age, breaks = 10,c(0,10,15,30), freq = TRUE,col = "blue")
 hist(Student$Age, breaks = 10,c(0,10,15,30), freq = TRUE,col = "blue",main  = "studet age")
+
+barplot(Student$Experience..Years.)
+y <- Student
+
