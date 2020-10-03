960b15a529b49e048e89ebb5e9233d2280350a26
raghu <raghumina11@gmail.com>
2020-10-03 11:28:06 +0530

    Create Varaible_datatype.r

---
+++ Varaible_datatype.r
@@ -0,0 +1,27 @@
+# Variables and data types 
+
+# Variables are temporary sotrage space 
+# We can change the values of variables anytime 
+
+a <- 10
+a 
+a <- 4
+a
+
+#----------------------------------------------------------#
+
+# data type 
+# example of data tpyes 
+
+#num <- 21,22,12
+#char <- "hello","tool"
+#logical <- TRUE, FALSE
+#complex <- 23i+1
+
+#-----------------------------------------------------------#
+
+# Assignment operator
+b = 1 
+b <- 1 
+
+
