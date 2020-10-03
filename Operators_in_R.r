85339b4743125cbeb82d3c803cfce891f312a771
raghu <raghumina11@gmail.com>
2020-10-03 11:52:10 +0530

    Update Operators_in_R.r

--- Operators_in_R.r
+++ Operators_in_R.r
@@ -24,5 +24,46 @@ a
 6 -> a
 a
 
+#----------------------------------------------------------------------------------#
 
+# Arithmatic operator
+# The basic arithmetic operations are addition, subtraction, multiplication, and division.
+# Arithmetic is performed according to an order of operations.
+
+num1 <- 4
+num2 <- 4
+
+num1 + num2    # addition operator
+num1 * num2   # multiply operator
+num1 / num2     # divide operator
+num1 %% num2      # modulo opeator
+num1 - num2      #V substract operator
+
+#-----------------------------------------------------------------------------------#
+
+# Relational operator
+# In computer science, a relational operator is a programming language construct 
+# or operator that tests or defines some kind of relation between two entities.
+# These include numerical equality and inequalities
+
+num1 < num2
+num1 > num2
+num1 == num2
+num1 != num2
+
+
+#----------------------------------------------------------------------------------#
+
+# Logical opeartor 
 # 
+
+
+
+
+
+
+
+
+
+
+
