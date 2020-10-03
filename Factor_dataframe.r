7bd92df19631eb42ddbe8321fdb3e66f477f9799
raghu <raghumina11@gmail.com>
2020-10-03 22:36:10 +0530

    Create Factor_dataframe.r

---
+++ Factor_dataframe.r
@@ -0,0 +1,11 @@
+# Factor and Data Frame in R 
+# Factors are the data objects which are used to categorize the data and store it as levels. 
+# They can store both strings and integers. They are useful in the columns which
+# have a limited number of unique values. Like "Male, "Female" and True, False etc.
+# They are useful in data analysis for statistical modeling.
+
+
+
+
+
+
