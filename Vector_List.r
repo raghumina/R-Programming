98c180ebaae1ac55aa7092361a8494ac28046a6e
raghu <raghumina11@gmail.com>
2020-10-03 21:28:08 +0530

    Create Vector_List.r

---
+++ Vector_List.r
@@ -0,0 +1,12 @@
+# Vector and List in R
+# They are the data types in R
+# R's basic data types are character, numeric, integer, complex, and logical.
+# R's basic data structures include the vector, list, matrix, data frame, and factors. 
+# Objects may have attributes, such as name, dimension, and class.
+
+
+# Vectors are homogenous single data structure
+# Vector is a basic data structure in R. It contains element of the same type. 
+# The data types can be logical, integer, double, character, complex or raw.
+# A vector's type can be checked with the typeof() function.
+
