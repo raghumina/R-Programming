662deb242d8d80b406cf9b2cf3507d95102e117d
raghu <raghumina11@gmail.com>
2020-10-03 11:39:05 +0530

    Create Operators_in_R.r

---
+++ Operators_in_R.r
@@ -0,0 +1,28 @@
+# Operators 
+# An operator in a programming language is a symbol that tells 
+# the compiler or interpreter to perform specific mathematical,
+# relational or logical operation and produce final result.
+
+# Different types of operators in R
+
+# Assignment operator
+# Arithmatic operator
+# Relational operator
+# Logical operator 
+
+#-----------------------------------------------------------------------------#
+
+# Assignment operator
+
+# <- 
+# = 
+
+a = 5
+a
+a <- 4
+a
+6 -> a
+a
+
+
+# 
