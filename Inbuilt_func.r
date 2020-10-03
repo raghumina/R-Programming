37817a4838544be18feac6fd67e8663340f83e0e
raghu <raghumina11@gmail.com>
2020-10-03 22:48:42 +0530

    Create Inbuilt_func.r

---
+++ Inbuilt_func.r
@@ -0,0 +1,20 @@
+# Inbuilt functions in R 
+# 
+View(iris)
+head(iris)     # to view first few rows and coloumn of the data frame
+tail(iris)    # to view last few data strucure of data frame
+str(iris)     # to know the structure of the data frame
+
+table(iris$Sepal.Length)   # to view specific coloumn of the data frame
+
+min(iris$Sepal.Length)    # to know minimum value
+max(iris$Sepal.Length)    # to know maximum value
+
+mean(iris$Sepal.Length)  # to know mean value
+
+range(iris$Sepal.Length)   # to find range min and max value
+
+
+
+
+  
