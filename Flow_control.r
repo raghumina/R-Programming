bb55718f46ea45016dfe25efa883dbf91cede2bb
raghu <raghumina11@gmail.com>
2020-10-04 08:40:28 +0530

    Update Flow_control.r

--- Flow_control.r
+++ Flow_control.r
@@ -2,6 +2,8 @@
 # Conditional statement 
 # 
 
+# if condition
+
 View(iris)
 if(iris$Sepal.Length[1]>4){
   print("Sepal length is greater than 4")
@@ -13,6 +15,43 @@ if(iris$Sepal.Length[1]<4){                 # this will not get an output becaus
 }
 
 
+# if else...
+
+if(iris$Sepal.Length[1]<4){              # here else condition will get printed 
+  print("Sepal length is less than 4")
+}else{
+  print("Sepal length is greater than 4")
+}
 
 
+#--------------------------------------------------------------------------------#
+
+# Looping Statement
+# for and while
+# will run the loop until condition gets true 
+
+vec1 <- 1:9
+for(i in vec1){
+  print(i + 5)
+}
+
+#----------------------------------------
+
+# while loop 
+
+i = 1 
+while(i < 10){
+  print(i + 5)
+  i = i + 1
+}
+
+
+#--------------------------------------------------------------------------------#
+
+# User defined function 
+# 
+add_5 <- function(x){
+  print(x + 5)
+}
+add_5(20)            # here x is 20 and result will be 25
 
