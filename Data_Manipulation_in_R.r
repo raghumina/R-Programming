0d28eb4b7a6fd051995ce260adf2a6d34d17822f
raghu <raghumina11@gmail.com>
2020-10-04 09:29:11 +0530

    Update Data_Manipulation_in_R.r

--- Data_Manipulation_in_R.r
+++ Data_Manipulation_in_R.r
@@ -22,3 +22,41 @@ laptop_3_6
 
 # or to select specific coloumns form a data frame
 
+laptop_price %>% select("ScreenResolution","Cpu") -> laptop_reso_cpu
+laptop_reso_cpu
+
+# to select specific coloumns which starts with specific alphabets 
+
+laptop_price %>% select(starts_with("P")) -> laptop_P
+laptop_P
+
+
+# to select specific coloumns which ends with specific alphabets 
+
+
+laptop_price %>% select(ends_with("s")) -> laptop_s
+laptop_s
+
+
+# Combining select and filter 
+# to filter out specific data 
+
+
+# FILTER 
+
+# Filter those laptops where company is dell 
+ 
+laptop_price %>% filter(Company=="Dell") -> laptop_Dell
+View(laptop_Dell)
+
+# Filter those laptops where company is dell and screen size in greater than 15 inch
+
+laptop_price %>% filter(Company=="Dell" & Inches> 15) -> laptop_Dell_screenS
+View(laptop_Dell_screenS)
+
+# Combining Select and Filter 
+
+# FOR Selecting specific coloumns with a specific conditions 
+
+laptop_price %>% select("Company","Product","Price_euros") %>% filter(Company =="Dell") -> laptop_final
+View(laptop_final)
