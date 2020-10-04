54d772da8136fb308d71bc31b95102f465808ba8
raghu <raghumina11@gmail.com>
2020-10-04 08:26:55 +0530

    Create Flow_control.r

---
+++ Flow_control.r
@@ -0,0 +1,18 @@
+# DECISION MAKING STATEMENT 
+# Conditional statement 
+# 
+
+View(iris)
+if(iris$Sepal.Length[1]>4){
+  print("Sepal length is greater than 4")
+}
+
+if(iris$Sepal.Length[1]<4){                 # this will not get an output because condition is false
+
+  print("Sepal length is greater than 4")
+}
+
+
+
+
+
